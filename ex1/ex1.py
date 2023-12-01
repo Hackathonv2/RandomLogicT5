7424574fca7d08940a3141197cfa6f4c5bc16d86
Antoine <me@antoinebellanger.fr>
2023-12-01 09:29:15 +0100

    Ex1 Done

---
+++ ex1/ex1.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == "__main__":
+    line = sys.stdin.readline()
+    try:
+        power = int(line)
+    except ValueError:
+        sys.exit(1)
+    for i in range(4):
+        if power % 3 == 0:
+            power /= 3
+        elif power % 2 == 0:
+            power /= 2
+        else:
+            power -= 1
+    sys.stdin.close()
+    print(power)
